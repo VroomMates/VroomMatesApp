62e5abb8ba22c3355473228a30eb3e9a8e2de17e
VroomMates <vroommatesinfo@gmail.com>
2016-11-09 01:52:28 +0000

    Just my test files

---
+++ VroomMates/f.js
@@ -0,0 +1,53 @@
+    var config = {
+		apiKey: "AIzaSyB3ocXC1s5sSMOT2ZV0snKdjBx85XsbPmA",
+		authDomain: "college-4ab84.firebaseapp.com",
+		databaseURL: "https://college-4ab84.firebaseio.com",
+		storageBucket: "college-4ab84.appspot.com",
+		messagingSenderId: "409094544332"
+	  };
+	  firebase.initializeApp(config);
+    /**
+     * Handles the sign up button press.
+     */
+    function handleSignUp() {
+      var email = document.getElementById('email').value;
+      var password = document.getElementById('password').value;
+      // Sign in with email and pass.
+      // [START createwithemail]
+	  var success=true;
+      firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // [START_EXCLUDE]
+        if (errorCode == 'auth/weak-password') {
+          alert('The password is too weak.');
+        } else {
+          alert(errorMessage);
+        }
+        console.log(error);
+		success=false;
+        // [END_EXCLUDE]
+      });
+      // [END createwithemail]
+    }
+	window.onload = function() {
+	alert(firebase.auth().currentUser);
+      if(firebase.auth().currentUser)
+	  {
+		window.location="home.html";
+	  }
+    };
+    /**
+     * initApp handles setting up UI event listeners and registering Firebase auth listeners:
+     *  - firebase.auth().onAuthStateChanged: This listener is called when the user is signed in or
+     *    out, and that is where we update the UI.
+     */
+    
+	
+	
+	//USER INFO SCRIPT
+	
+	
+	
+	
